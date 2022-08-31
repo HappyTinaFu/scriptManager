8bbe385398b85423b89ae5b47f870c89e4e214a8
付文兰(26043711) <fuwenlan001@ke.com>
2022-08-31 12:03:36 +0800

    maven-assembly-plugin test by mvn package assembly:single

--- src/main/java/com/tinafu/App.java
+++ src/main/java/com/tinafu/App.java
@@ -1,5 +1,11 @@
 package com.tinafu;
 
+import javax.script.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.StringWriter;
+
 /**
  * Hello world!
  *
@@ -9,5 +15,19 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        StringWriter output = new StringWriter();
+        ScriptContext context = new SimpleScriptContext();
+        context.setWriter(output);
+
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("python");
+        try {
+            engine.eval(new FileReader("hello.py"), context);
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println(output.toString());
     }
 }
